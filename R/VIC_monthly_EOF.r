b19af8c6ac707281823452963470ed1f43e70e53
kheil <kheilman@nd.edu>
2015-04-02 21:31:24 -0400

    added write.table functions to save PC and EOF matrices

--- R/VIC_monthly_EOF.r
+++ R/VIC_monthly_EOF.r
@@ -10,43 +10,24 @@ library(sp)
 ##Pacific Oscillation index data for later use
 #this data is from http://www.cpc.ncep.noaa.gov/data/teledoc/pna.shtml
 PNO <- read.table("PNA.txt", header=TRUE, sep="\t")
-
 #########################################3
-#code that is not needed (a long way of doing things)
+#Loading Monthly data from chun-mei's VIC run
 ################################
 # list the files to process that are in the current directory with the pattern beginng with "FLUX_"
 filesToProcess <- dir(pattern = "Monthly_Flux_.*")
 
 file_list<-filesToProcess
-##2. Iterate over each of those file names with lapply
-#listOfFiles <- lapply(filesToProcess, function(x) read.table(x, header = FALSE))
-
-##3. Select columns x and y2 from each of the objects in our list
-#listOfFiles <- lapply(listOfFiles, function(z) cbind(rownames(z),z[,4])
-
-##NOTE: you can combine steps 2 and 3 by passing in the colClasses parameter to read.table.
-#That code would be:
-#listOfFiles <- lapply(filesToProcess, function(x) read.table(x, header = TRUE
- 
-# order of variables for Monthly_Flux.* files: Year Month precipitation, evaporation, wind speed, surface temperature,  air temperature, and soil layer's soil moisture 
-
-##this worked to get column three from all flux files into columns using cbind
+##extract column of interest from all flux files into columns using cbind
 dataset<-do.call("cbind", lapply(file_list,FUN=function(files){read.table(files)$V3}))
 colnames(dataset)<-substr(file_list, 14, 30) #this takes the latlon from each file makes it the header for each row
 
 latlong<-cbind(substr(file_list,14,21),substr(file_list,23,30)) #create latlong vector from file names
 
-##Now that data is formatted properly, can do analyses: EOF 
-
-#timeseriesplot for 1 gridcell
-#precip.time<-ts(dataset[,1], freq = 12, start=c(1950,1))
-#plot.ts(evap.time)
 
 ######################
 ##EOF Analysis
 #####################
 #express the space-time data as a 2D matrix with "M" rows (space) by "N" columns(time)
-
 #transpose our data so that time is represented in the columns and space in rows
 
 A <- t(as.matrix(dataset))  #convert to matrix and transpose
@@ -56,11 +37,6 @@ A <- t(as.matrix(dataset))  #convert to matrix and transpose
 ##This creates an "anomoly" matrix
 ltmeans <- apply(A, 1, mean)   #calculate the longterm means for each row and puts it in a list
 X <- A - ltmeans   #subtract longterm means from the matrix A
-
-#dont need this if we use svd()
-##Calculate the covariance matrix, C for the anomoly matrix X
-#C_1 <- X %*% t(X) #by hand
-#C <- cov(X) #using function cov
 
 ## Calculate the eigenvectors (E) and Eigen Values EV of the covariance matrix C
 esv <- svd(X) #svd analysis of a matrix returns a lise with d, u, and v objections
@@ -79,9 +55,9 @@ uta <- t(U) %*% X
 A <- U[,1:2] %*% z[1:2,] # this gives the first 2 rows in the first column as non-zero, but rounding, they are zero
 
 #the Principle components here now represent time  series of length N
-plot(z[1,],type='l')  #time series explaining the most variation in the data
-plot(z[2,],type='l')  #time series for 2nd EOF
-
+plot(z[1,], type='l')  #time series explaining the most variation in the data
+plot(z[2,], type='l')  #time series for 2nd EOF
+plot(z[3,], type='l')
 
 #create separate objects for eofs 1-3
 eof1 <- data.frame(cbind(as.numeric(latlong[,1]),as.numeric(latlong[,2]),U[,1]))
@@ -92,6 +68,7 @@ eof3 <- data.frame(cbind(as.numeric(latlong[,1]), as.numeric(latlong[,2]), U[,3]
 colnames(eof3) <- c("lat", "lon","eof3")
 
 write.table(U,"precipU.txt",sep='\t')
+write.table(Z, "precipPCs.txt", sep='\t')
 
 ##make the data spatial points using coordinates() from the sp package
 coordinates(eof1) <-~lon+lat
@@ -173,8 +150,8 @@ eof2 <- data.frame(cbind(as.numeric(latlong[,1]),as.numeric(latlong[,2]),U[,2]))
 colnames(eof2) <- c("lat", "lon","eof2")
 eof3 <- data.frame(cbind(as.numeric(latlong[,1]), as.numeric(latlong[,2]), U[,3]))
 colnames(eof3) <- c("lat", "lon","eof3")
-write.table(U, "soilm2U.txt", sep="\t") # write a table with EOFS
-
+write.table(U, "soilm1U.txt", sep="\t") # write a table with EOFS
+write.table(z, "soilm1Z.txt", sep="\t")
 ##make the data spatial points using coordinates() from the sp package
 coordinates(eof1) <-~lon+lat
 coordinates(eof2) <-~lon+lat
@@ -253,6 +230,7 @@ plot(z[1,],type='l')  #time series explaining the most variation in the data
 plot(z[2,],type='l')  #time series for 2nd EOF
 
 write.table(U, "soilm2U.txt", sep="\t")
+write.table(z, "soilm2Z.txt", sep="\t")
 #create separate objects for eofs 1-3
 eof1 <- data.frame(cbind(as.numeric(latlong[,1]),as.numeric(latlong[,2]),U[,1]))
 colnames(eof1) <- c("lat", "lon","eof1")
@@ -328,6 +306,7 @@ plot(z[1,],type='l')  #time series explaining the most variation in the data
 plot(z[2,],type='l')  #time series for 2nd EOF
 
 write.table(U, "soilm3U.txt", sep="\t")
+write.table(z, "soilm3Z.txt", sep="\t")
 #create separate objects for eofs 1-3
 eof1 <- data.frame(cbind(as.numeric(latlong[,1]),as.numeric(latlong[,2]),U[,1]))
 colnames(eof1) <- c("lat", "lon","eof1")
@@ -350,7 +329,10 @@ soilm3.eof3 <- spplot(eof3, main = "EOF3 Soil moisture layer3")
 
 ###save the soil moisture EOF's to a pdf
 
-pdf("soilmoistureEOFs.pdf")
+pdf("EOFmaps.pdf")
+precip.eof1
+precip.eof2
+precip.eof3
 soilm1.eof1
 soilm1.eof2
 soilm1.eof3
